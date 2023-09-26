0a6ebb1feb7fe713c6543b9daaeb93847c197e31
HiraethAwaits <146136772+HiraethAwaits@users.noreply.github.com>
2023-09-26 14:07:58 -0700

    Update git-config.sh

--- scripts/git-config.sh
+++ scripts/git-config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Enable us to push, through email privacy features
-git config --global 146136772+HiraethAwaits@users.noreply.github.com.
+git config --global user.email 146136772+HiraethAwaits@users.noreply.github.com.
